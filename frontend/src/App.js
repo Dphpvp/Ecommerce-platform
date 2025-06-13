471b1c2a19b1165427e6b7cc1c8e0af65d1dfc5d
Dph <razvan.mare@live.com>
2025-06-13 10:24:47 +0300

    Update with dark mode 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
 import { ToastProvider } from './components/toast';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 import Header from './components/header';
 import Footer from './components/Footer';
