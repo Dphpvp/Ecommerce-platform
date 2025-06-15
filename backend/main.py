29318d794454132baf1fa4a62d6e32768c7d6937
Razvan Mare <razvan.mare@live.com>
2025-06-15 17:45:32 +0300

    Update pw reset

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"]
+    expose_headers=["*"],
 )
 
 # Stripe configuration
