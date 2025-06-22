2cf45f5cd6a067fb230b7cb72f671d1e9dee0e84
Razvan Mare <razvan.mare@live.com>
2025-06-22 11:21:25 +0300

    Theme update again 6

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useCart } from '../contexts/CartContext';
 import { useAuth } from '../contexts/AuthContext';
 import { useToastContext } from './toast';
-import Modal from './Modal';
+import Modal from '../components/Modal';
 
 // Random fabric/tailoring images from Unsplash
 const fabricImages = [
