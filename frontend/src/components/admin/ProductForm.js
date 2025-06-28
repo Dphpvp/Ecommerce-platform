c3b197eff02ec85f03dcc4c595299340c5483fa1
Razvan Mare <razvan.mare@live.com>
2025-06-28 16:30:53 +0300

    Theme update 10

--- frontend/src/components/admin/ProductForm.js
+++ frontend/src/components/admin/ProductForm.js
@@ -3,7 +3,7 @@ import { useAuth } from '../../contexts/AuthContext';
 import { useToastContext } from '../toast';
 import SecureForm from '../SecureForm';
 import { csrfManager } from '../../utils/csrf';
-import '../pages/ProductForm.css';
+import '../../pages/ProductForm.css';
 
 const ProductForm = ({
   product,
