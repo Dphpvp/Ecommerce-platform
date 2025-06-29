448b03dfeaf84c8933e8280caad3254d55c3c479
Dph <razvan.mare@live.com>
2025-06-29 18:16:47 +0300

    Theme update css 2

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ import {
 import { useScrollAnimation } from '../hooks/useParallax';
 import { preloadImages } from '../utils/parallax';
 import { parallaxConfig } from '../config/parallax';
-import './styles/main.css';
+
 
 const API_BASE = process.env.REACT_APP_API_BASE_URL;
 
