1c00584e2adcdd31a168df9374c3298bcb36b2db
Razvan Mare <razvan.mare@live.com>
2025-07-02 21:26:06 +0300

    Theme update ffs

--- frontend/src/components/LuxuryNavigation/LuxuryNavigation.js
+++ frontend/src/components/LuxuryNavigation/LuxuryNavigation.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
 import { useCart } from '../../contexts/CartContext';
-import '../../styles/components/LuxuryNavigation.css';
+
 
 const LuxuryNavigation = () => {
   const [activeIndex, setActiveIndex] = useState(0);
