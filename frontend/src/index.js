4d15c7619fafe20c55b52452460111ea0b8362d2
Dph <razvan.mare@live.com>
2025-07-03 10:30:38 +0300

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 // CSS imports in correct order for luxury theme
 import './index.css';
 import './styles/main.css';
+import './styles/styles.css'
 import './App.css';
 
 import App from './App';
