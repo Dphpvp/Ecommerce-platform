63817935f0455c547e01122e7b09459667451be1
Dph <razvan.mare@live.com>
2025-07-29 01:23:54 +0300

    Contact update 2

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -263,7 +263,7 @@ const Products = () => {
                 onClick={() => setViewMode('list')}
                 title="List View"
               >
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                   <line x1="8" y1="6" x2="21" y2="6"/>
                   <line x1="8" y1="12" x2="21" y2="12"/>
                   <line x1="8" y1="18" x2="21" y2="18"/>
