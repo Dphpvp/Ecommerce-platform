53750f09a728ae795915e193137f850efa2c8ba7
Dphpvp <razvan.mare@live.com>
2025-07-28 10:57:30 +0300

    update style

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -62,7 +62,7 @@ const Navigation = () => {
       <div className="header-container">
         {/* Brand */}
         <Link to="/" className="header-brand">
-          EcoFashion
+          Vergi Shop
         </Link>
 
         {/* Main Navigation */}
@@ -116,7 +116,7 @@ const Navigation = () => {
           </button>
 
           {/* Wishlist */}
-          <Link to="/wishlist" className="wishlist-button">
+          <Link to="/wishlist" className="search-toggle">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
             </svg>
